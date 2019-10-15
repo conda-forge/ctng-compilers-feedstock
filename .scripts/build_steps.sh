bcca4359ab57567e9853d2908b1c38964aa9c124
conda-forge-admin <pelson.pub+conda-forge@gmail.com>
2019-10-15 22:45:58 +0000

    MNT: Re-rendered with conda-build 3.18.9, conda-smithy 3.6.0, and conda-forge-pinning 2019.10.11

--- .scripts/build_steps.sh
+++ .scripts/build_steps.sh
@@ -31,7 +31,7 @@ source run_conda_forge_build_setup
 # "recipe/yum_requirements.txt" file. After updating that file,
 # run "conda smithy rerender" and this line will be updated
 # automatically.
-/usr/bin/sudo -n yum install -y wget m4 help2man patch gcc-gfortran gcc-c++ rsync sed findutils make file
+/usr/bin/sudo -n yum install -y wget m4 help2man patch gcc-gfortran gcc-c++ rsync sed findutils make file autoconf automake
 
 
 # make the build number clobber
