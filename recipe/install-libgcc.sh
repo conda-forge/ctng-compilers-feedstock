b1b2e9483d8a084592dac8bce23ff049927040d8
Matthew R Becker <becker.mr@gmail.com>
2020-01-04 09:07:23 -0600

    try again

--- recipe/install-libgcc.sh
+++ recipe/install-libgcc.sh
@@ -21,14 +21,14 @@ pushd ${SRC_DIR}/.build/${CHOST}/build/build-cc-gcc-final/
                  ${CHOST}/${lib}/libtool
     fi
   done
-  for lib in libatomic libquadmath libitm libvtv libsanitizer/{a,l,ub,t}san; do
+  for lib in libatomic libgomp libquadmath libitm libvtv libsanitizer/{a,l,ub,t}san; do
     if [[ -d ${CHOST}/${lib} ]]; then
       make -C ${CHOST}/${lib} prefix=${PREFIX} install-toolexeclibLTLIBRARIES
       make -C ${CHOST}/${lib} prefix=${PREFIX} install-nodist_fincludeHEADERS || true
     fi
   done
 
-  for lib in libquadmath; do
+  for lib in libquadmath libgomp; do
     if [[ -d ${CHOST}/${lib} ]]; then
       make -C ${CHOST}/${lib} prefix=${PREFIX} install-info
     fi
@@ -39,12 +39,18 @@ popd
 mkdir -p ${PREFIX}/lib
 mv ${PREFIX}/${CHOST}/lib/* ${PREFIX}/lib
 
-for lib in libatomic libquadmath libitm libvtv lib{a,l,ub,t}san; do
+for lib in libatomic libgomp libquadmath libitm libvtv lib{a,l,ub,t}san; do
   symtargets=$(find ${PREFIX}/lib -name "${lib}.so*")
   for symtarget in ${symtargets}; do
     symtargetname=$(basename ${symtarget})
     ln -s ${PREFIX}/lib/${symtargetname} ${PREFIX}/${CHOST}/sysroot/lib/${symtargetname}
   done
+done
+
+# remove parts of openmp libs that we do not need
+for tgt in libgomp.so.1.0.0 libgomp.so.1 libgomp.so; do
+    rm ${PREFIX}/lib/${tgt}
+    rm ${PREFIX}/${CHOST}/sysroot/lib/${tgt}
 done
 
 # TODO remove for conda build > 3.18.11
--- recipe/install-libgomp.sh
+++ recipe/install-libgomp.sh
@@ -7,39 +7,32 @@ CHOST=$(${SRC_DIR}/.build/*-*-*-*/build/build-cc-gcc-final/gcc/xgcc -dumpmachine
 export PATH=${SRC_DIR}/gcc_built/bin:${SRC_DIR}/.build/${CHOST}/buildtools/bin:${SRC_DIR}/.build/tools/bin:${PATH}
 
 pushd ${SRC_DIR}/.build/${CHOST}/build/build-cc-gcc-final/
+  # here we install to dummy temp prefix and then pull out only the lib we need
+  mkdir -p ${PREFIX}/gomp_test_prefix
+  mkdir -p ${PREFIX}/gomp_test_prefix/${CHOST}/sysroot/lib || true
 
-  mkdir -p ${PREFIX}/${CHOST}/sysroot/lib || true
   for lib in libgomp; do
     if [[ -d ${CHOST}/${lib} ]]; then
-      make -C ${CHOST}/${lib} prefix=${PREFIX} install-toolexeclibLTLIBRARIES
-      make -C ${CHOST}/${lib} prefix=${PREFIX} install-nodist_fincludeHEADERS || true
+      make -C ${CHOST}/${lib} prefix=${PREFIX}/gomp_test_prefix install-toolexeclibLTLIBRARIES
+      make -C ${CHOST}/${lib} prefix=${PREFIX}/gomp_test_prefix install-nodist_fincludeHEADERS || true
     fi
   done
 
   for lib in libgomp; do
     if [[ -d ${CHOST}/${lib} ]]; then
-      make -C ${CHOST}/${lib} prefix=${PREFIX} install-info
+      make -C ${CHOST}/${lib} prefix=${PREFIX}/gomp_test_prefix install-info
     fi
   done
 
 popd
 
 mkdir -p ${PREFIX}/lib
-mv ${PREFIX}/${CHOST}/lib/* ${PREFIX}/lib
+mkdir -p ${PREFIX}/${CHOST}/sysroot/lib
+cp ${PREFIX}/gomp_test_prefix/${CHOST}/lib/libgomp.so.1.0.0 ${PREFIX}/lib/libgomp.so.1.0.0
+ln -s ${PREFIX}/lib/libgomp.so.1.0.0 ${PREFIX}/${CHOST}/sysroot/lib/libgomp.so.1.0.0
 
-for lib in libgomp; do
-  symtargets=$(find ${PREFIX}/lib -name "${lib}.so*")
-  for symtarget in ${symtargets}; do
-    symtargetname=$(basename ${symtarget})
-    ln -s ${PREFIX}/lib/${symtargetname} ${PREFIX}/${CHOST}/sysroot/lib/${symtargetname}
-  done
-done
-
-# remove parts of openmp libs that we do not need
-for tgt in libgomp.so.1 libgomp.so; do
-    rm ${PREFIX}/lib/${tgt}
-    rm ${PREFIX}/${CHOST}/sysroot/lib/${tgt}
-done
+# remove the dummy prefix
+rm -rf ${PREFIX}/gomp_test_prefix
 
 # no static libs
 find ${PREFIX}/lib -name "*\.a" -exec rm -rf {} \;
@@ -50,4 +43,4 @@ rm -rf ${PREFIX}/lib/gcc
 
 # Install Runtime Library Exception
 install -Dm644 ${SRC_DIR}/.build/src/gcc-${ctng_gcc}/COPYING.RUNTIME \
-        ${PREFIX}/share/licenses/gcc-libs/RUNTIME.LIBRARY.EXCEPTION
+        ${PREFIX}/share/licenses/gcc-libs/RUNTIME.LIBRARY.EXCEPTION.gomp_copy
