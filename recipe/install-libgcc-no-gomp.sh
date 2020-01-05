52ac220b0297d62b5bbbf6a32f26a98cfa6b636e
Matthew R Becker <becker.mr@gmail.com>
2020-01-04 21:52:19 -0600

    try this

--- recipe/install-libgcc-no-gomp.sh
+++ recipe/install-libgcc-no-gomp.sh
@@ -3,7 +3,15 @@ source ${RECIPE_DIR}/install-libgcc.sh
 # remove and relink things
 rm -f ${PREFIX}/lib/libgomp.so
 rm -f ${PREFIX}/${CHOST}/sysroot/lib/libgomp.so
+rm -f ${PREFIX}/lib/libgomp.so.${libgomp_ver:0:1}
+rm -f ${PREFIX}/${CHOST}/sysroot/lib/libgomp.so.${libgomp_ver:0:1}
+rm -f ${PREFIX}/${CHOST}/sysroot/lib/libgomp.so.${libgomp_ver}
 
 # make the right links
 ln -s ${PREFIX}/lib/libgomp.so.${libgomp_ver:0:1} ${PREFIX}/lib/libgomp.so
 ln -s ${PREFIX}/lib/libgomp.so ${PREFIX}/${CHOST}/sysroot/lib/libgomp.so
+
+ln -s ${PREFIX}/lib/libgomp.so.${libgomp_ver} ${PREFIX}/lib/libgomp.so.${libgomp_ver:0:1}
+ln -s ${PREFIX}/lib/libgomp.so.${libgomp_ver:0:1} ${PREFIX}/${CHOST}/sysroot/lib/libgomp.so.${libgomp_ver:0:1}
+
+ln -s ${PREFIX}/lib/libgomp.so.${libgomp_ver} ${PREFIX}/${CHOST}/sysroot/lib/libgomp.so.${libgomp_ver}
--- recipe/install-libgomp.sh
+++ recipe/install-libgomp.sh
@@ -1,5 +1,18 @@
-set -e -x
+source ${RECIPE_DIR}/install-libgcc.sh
+
+# stash what we need and rm -rf the rest
+tmp_dir=$(mktemp -d -t ci-XXXXXXXXXX)
+cp ${PREFIX}/lib/libgomp.so.${libgomp_ver} ${tmp_dir}/libgomp.so.${libgomp_ver}
+rm -rf ${PREFIX}/*
+
+# copy back and make the right links
+mkdir -p ${PREFIX}/lib
+cp ${tmp_dir}/libgomp.so.${libgomp_ver} ${PREFIX}/lib/libgomp.so.${libgomp_ver}
+ln -s ${PREFIX}/lib/libgomp.so.${libgomp_ver} ${PREFIX}/${CHOST}/sysroot/lib/libgomp.so.${libgomp_ver}
 
 # Install Runtime Library Exception
 install -Dm644 ${SRC_DIR}/.build/src/gcc-${ctng_gcc}/COPYING.RUNTIME \
         ${PREFIX}/share/licenses/gcc-libs/RUNTIME.LIBRARY.EXCEPTION.gomp_copy
+
+# remove the copy in libgcc
+rm -f ${PREFIX}/share/licenses/gcc-libs/RUNTIME.LIBRARY.EXCEPTION
