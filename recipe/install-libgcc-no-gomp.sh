df72550254bf6cef6d12fb05bd09440ce16bcfeb
Matthew R Becker <becker.mr@gmail.com>
2020-01-05 09:59:53 -0600

    more bugz

--- recipe/install-libgcc-no-gomp.sh
+++ recipe/install-libgcc-no-gomp.sh
@@ -1,6 +1,17 @@
+set -e -x
+
+CHOST=$(${SRC_DIR}/.build/*-*-*-*/build/build-cc-gcc-final/gcc/xgcc -dumpmachine)
+
+# we have to remove existing links/files so that the libgcc install works
+rm -rf ${PREFIX}/lib/*
+rm -rf ${PREFIX}/${CHOST}/sysroot/lib/*
+rm -rf ${PREFIX}/share/*
+
+# now run install of libgcc
+# this reinstalls the wrong symlinks for openmp
 source ${RECIPE_DIR}/install-libgcc.sh
 
-# remove and relink things
+# remove and relink things for openmp
 rm -f ${PREFIX}/lib/libgomp.so
 rm -f ${PREFIX}/${CHOST}/sysroot/lib/libgomp.so
 rm -f ${PREFIX}/lib/libgomp.so.${libgomp_ver:0:1}
--- recipe/install-libgomp.sh
+++ recipe/install-libgomp.sh
@@ -16,3 +16,5 @@ ln -s ${PREFIX}/lib/libgomp.so.${libgomp_ver} ${PREFIX}/${CHOST}/sysroot/lib/lib
 # Install Runtime Library Exception
 install -Dm644 ${SRC_DIR}/.build/src/gcc-${ctng_gcc}/COPYING.RUNTIME \
         ${PREFIX}/share/licenses/gcc-libs/RUNTIME.LIBRARY.EXCEPTION.gomp_copy
+
+rm -rf ${tmp_dir}
