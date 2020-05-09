e0fed501382c1090a7bfc08b17f6cb7608de645d
Matthew R Becker <becker.mr@gmail.com>
2020-05-09 13:26:33 -0500

    try again

--- recipe/install-gcc.sh
+++ recipe/install-gcc.sh
@@ -215,19 +215,22 @@ set +x
 # Strip executables, we may want to install to a different prefix
 # and strip in there so that we do not change files that are not
 # part of this package.
-pushd ${PREFIX}
-  _files=$(find . -type f)
-  for _file in ${_files}; do
-    _type="$( file "${_file}" | cut -d ' ' -f 2- )"
-    case "${_type}" in
-      *script*executable*)
-      ;;
-      *executable*)
-      ${SRC_DIR}/gcc_built/bin/${CHOST}-strip --strip-all -v "${_file}" || :
-      ;;
-    esac
-  done
-popd
+#################
+# I cannot get the try except with || to work with strip so commenting out for
+# now
+# pushd ${PREFIX}
+#   _files=$(find . -type f)
+#   for _file in ${_files}; do
+#     _type="$( file "${_file}" | cut -d ' ' -f 2- )"
+#     case "${_type}" in
+#       *script*executable*)
+#       ;;
+#       *executable*)
+#       ${SRC_DIR}/gcc_built/bin/${CHOST}-strip --strip-all -v "${_file}" || :
+#       ;;
+#     esac
+#   done
+# popd
 
 #${PREFIX}/bin/${CHOST}-gcc "${RECIPE_DIR}"/c11threads.c -std=c11
 
