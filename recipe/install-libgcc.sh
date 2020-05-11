4f7ea617498f3f9ab10bae5646613fd6b8d6b332
Matthew R Becker <becker.mr@gmail.com>
2020-05-11 14:44:15 -0500

    try again

--- recipe/install-libgcc.sh
+++ recipe/install-libgcc.sh
@@ -40,6 +40,7 @@ for lib in libatomic libgomp libquadmath libitm libvtv lib{a,l,ub,t}san; do
   symtargets=$(find ${PREFIX}/lib -name "${lib}.so*")
   for symtarget in ${symtargets}; do
     symtargetname=$(basename ${symtarget})
+    rm -f ${PREFIX}/${CHOST}/sysroot/lib/${symtargetname}
     ln -s ${PREFIX}/lib/${symtargetname} ${PREFIX}/${CHOST}/sysroot/lib/${symtargetname}
   done
 done
