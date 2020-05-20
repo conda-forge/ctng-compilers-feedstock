fb5ffb8219a527193992cb44b647d848fb5c7080
Matthew R Becker <becker.mr@gmail.com>
2020-05-20 14:53:04 -0500

    MNT: Re-rendered with conda-build 3.19.2, conda-smithy 3.7.3, and conda-forge-pinning 2020.05.20.09.40.28

--- recipe/install-duma.sh
+++ recipe/install-duma.sh
@@ -6,16 +6,4 @@ pushd ${SRC_DIR}/.build/${CHOST}/build/build-duma
   make prefix=${PREFIX} HOSTCC=$(uname -m)-build_pc-linux-gnu-gcc CC=${CHOST}-gcc CXX=${CHOST}-g++ RANLIB=${CHOST}-ranlib OS=linux DUMA_CPP=1 install
 popd
 
-if [[ "${ctng_cpu_arch}" == "x86_64" ]]; then
-  old_vendor="-conda_cos6-linux-gnu-"
-else
-  old_vendor="-conda_cos7-linux-gnu-"
-fi
-
-for exe in `ls ${PREFIX}/bin/*-conda-linux-gnu-*`; do
-  nm=`basename ${exe}`
-  new_nm=${nm/"-conda-linux-gnu-"/${old_vendor}}
-  if [ ! -f ${PREFIX}/bin/${new_nm} ]; then
-    ln -s ${exe} ${PREFIX}/bin/${new_nm}
-  fi
-done
+source ${RECIPE_DIR}/make_tool_links.sh
--- recipe/install-gdb.sh
+++ recipe/install-gdb.sh
@@ -31,16 +31,4 @@ pushd ${PREFIX}
   done
 popd
 
-if [[ "${ctng_cpu_arch}" == "x86_64" ]]; then
-  old_vendor="-conda_cos6-linux-gnu-"
-else
-  old_vendor="-conda_cos7-linux-gnu-"
-fi
-
-for exe in `ls ${PREFIX}/bin/*-conda-linux-gnu-*`; do
-  nm=`basename ${exe}`
-  new_nm=${nm/"-conda-linux-gnu-"/${old_vendor}}
-  if [ ! -f ${PREFIX}/bin/${new_nm} ]; then
-    ln -s ${exe} ${PREFIX}/bin/${new_nm}
-  fi
-done
+source ${RECIPE_DIR}/make_tool_links.sh
