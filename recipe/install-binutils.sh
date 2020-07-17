9fae83cffed4401647c4db3da604b6dee2698b61
Matthew R Becker <becker.mr@gmail.com>
2020-07-17 06:11:22 -0500

    ENH add extra sysroot deps to mark builds

--- recipe/install-binutils.sh
+++ recipe/install-binutils.sh
@@ -7,6 +7,12 @@ pushd ${SRC_DIR}/.build/${CHOST}/build/build-binutils-host-*
   make prefix=${PREFIX} install-strip
 popd
 
+# clean up the sysroot
+for tool in ar as ld ld.bfd ld.gold nm objcopy objdump ranlib readelf stri; do
+  rm -rf $PREFIX/$CHOST/bin/$tool
+  ln -s $PREFIX/bin/$CHOST-$tool $PREFIX/$CHOST/bin/$tool || true;
+done
+
 # Copy the liblto_plugin.so from the build tree. This is something of a hack and, on OSes other
 # than the build OS, may cause segfaults. This plugin is used by gcc-ar, gcc-as and gcc-ranlib.
 pushd ${SRC_DIR}/.build/*-*-*-*/build/build-cc-gcc-core-pass-2/gcc/
