194ab22710ea1b2dd342aa84c365d7384acd6c97
Isuru Fernando <isuruf@gmail.com>
2024-04-21 19:53:22 -0500

    fix symlinks

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,38 +5,6 @@ set -ex
 # ensure patch is applied
 grep 'conda-forge:: allow' gcc/gcc.c*
 
-get_cpu_arch() {
-  local CPU_ARCH
-  if [[ "$1" == *"-64" ]]; then
-    CPU_ARCH="x86_64"
-  elif [[ "$1" == *"-ppc64le" ]]; then
-    CPU_ARCH="powerpc64le"
-  elif [[ "$1" == *"-aarch64" ]]; then
-    CPU_ARCH="aarch64"
-  elif [[ "$1" == *"-s390x" ]]; then
-    CPU_ARCH="s390x"
-  else
-    echo "Unknown architecture"
-    exit 1
-  fi
-  echo $CPU_ARCH
-}
-
-get_triplet() {
-  if [[ "$1" == linux-* ]]; then
-    echo "$(get_cpu_arch $1)-conda-linux-gnu"
-  elif [[ "$1" == osx-64 ]]; then
-    echo "x86_64-apple-darwin13.4.0"
-  elif [[ "$1" == osx-arm64 ]]; then
-    echo "arm64-apple-darwin20.0.0"
-  elif [[ "$1" == win-64 ]]; then
-    echo "x86_64-w64-mingw32"
-  else
-    echo "unknown platform"
-    exit 1
-  fi
-}
-
 if [[ "$channel_targets" == *conda-forge* && "${build_platform}" == "${target_platform}" ]]; then
   # Use new compilers instead of relying on ones from the docker image
   conda create -p $SRC_DIR/cf-compilers gcc gfortran gxx binutils -c conda-forge --yes --quiet
@@ -50,9 +18,7 @@ if [[ "$channel_targets" == *conda-forge* ]]; then
   GCC_CONFIGURE_OPTIONS+=(--with-bugurl="https://github.com/conda-forge/ctng-compilers-feedstock/issues/new/choose")
 fi
 
-export BUILD="$(get_triplet $build_platform)"
-export HOST="$(get_triplet $target_platform)"
-export TARGET="$(get_triplet $cross_target_platform)"
+source $RECIPE_DIR/get_cpu_arch.sh
 
 for tool in addr2line ar as c++filt gcc g++ ld nm objcopy objdump ranlib readelf size strings strip; do
   if [[ ! -f $BUILD_PREFIX/bin/$BUILD-$tool ]]; then
