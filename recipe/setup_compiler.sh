e43ef9ba7e044eac75af49bb6a975ec0a54a240c
Isuru Fernando <isuruf@gmail.com>
2024-04-22 19:08:52 -0500

    more cleanup

--- recipe/setup_compiler.sh
+++ recipe/setup_compiler.sh
@@ -1,22 +1,16 @@
 #!/bin/bash
 
 if [[ ! -d  $SRC_DIR/cf-compilers ]]; then
-  if [[ "${build_platform}" == "${target_platform}" ]]; then
-    # Use new compilers instead of relying on ones from the docker image
-    conda create -p $SRC_DIR/cf-compilers gcc gfortran gxx binutils -c conda-forge --yes --quiet
-  fi
-
-  if [[ "${build_platform}" != "${target_platform}" ]]; then
-    conda create -p $SRC_DIR/cf-compilers -c conda-forge \
-      "binutils_impl_${target_platform}=${binutils_version}.*" \
-      "gcc_impl_${target_platform}=${gcc_version}.*" \
-      "gxx_impl_${target_platform}=${gcc_version}.*" \
-      "binutils_impl_${cross_target_platform}=${binutils_version}.*" \
-      "gcc_impl_${cross_target_platform}=${gcc_version}.*" \
-      "gxx_impl_${cross_target_platform}=${gcc_version}.*" \
-      "gfortran_impl_${cross_target_platform}=${gcc_version}.*" \
-      "${sysroot_name}_${cross_target_platform}=${sysroot_version}.*"
-  fi
+    conda create -p $SRC_DIR/cf-compilers -c conda-forge --yes --quiet \
+      "binutils_impl_${build_platform}" \
+      "gcc_impl_${build_platform}" \
+      "gxx_impl_${build_platform}" \
+      "gfortran_impl_${build_platform}" \
+      "binutils_impl_${target_platform}=${binutils_version}" \
+      "gcc_impl_${target_platform}" \
+      "gxx_impl_${target_platform}" \
+      "gfortran_impl_${target_platform}" \
+      "sysroot_${target_platform}=${c_stdlib_version}"
 fi
 
 export PATH=$SRC_DIR/cf-compilers/bin:$PATH
