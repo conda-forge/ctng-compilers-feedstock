58a2d498d6ad9bd12492d4dd8cc924f06952e754
Isuru Fernando <isuruf@gmail.com>
2024-05-13 09:59:51 -0500

    fix libgfortran.dll.a

--- recipe/install-gfortran.sh
+++ recipe/install-gfortran.sh
@@ -43,8 +43,12 @@ install -Dm644 $SRC_DIR/build-finclude/$PREFIX/lib/gcc/${CHOST}/${gcc_version}/i
 install -Dm644 $SRC_DIR/COPYING.RUNTIME \
         ${PREFIX}/share/licenses/gcc-fortran/RUNTIME.LIBRARY.EXCEPTION
 
-if [[ "${target_platform}" != "${cross_target_platform}" && ${triplet} == *linux* ]]; then
-  cp -f --no-dereference ${SRC_DIR}/build/${CHOST}/libgfortran/.libs/libgfortran*.so* ${PREFIX}/${CHOST}/lib/
+if [[ "${target_platform}" != "${cross_target_platform}" ]]; then
+  if [[ ${triplet} == *linux* ]]; then
+    cp -f --no-dereference ${SRC_DIR}/build/${CHOST}/libgfortran/.libs/libgfortran*.so* ${PREFIX}/${CHOST}/lib/
+  else
+    cp -f --no-dereference ${SRC_DIR}/build/${CHOST}/libgfortran/.libs/libgfortran.dll.a ${PREFIX}/${CHOST}/lib/
+  fi
 fi
 cp -f --no-dereference ${SRC_DIR}/build/${CHOST}/libgfortran/.libs/libgfortran.a ${PREFIX}/${CHOST}/lib/
 
