5b8b286b4718c34f474a5d333127818654d3a1e2
Isuru Fernando <isuruf@gmail.com>
2024-06-11 00:43:13 +0000

    install conda-build using pip

--- .scripts/build_steps.sh
+++ .scripts/build_steps.sh
@@ -43,6 +43,7 @@ setup_conda_rc "${FEEDSTOCK_ROOT}" "${RECIPE_ROOT}" "${CONFIG_FILE}"
 
 source run_conda_forge_build_setup
 
+pip install git+https://github.com/conda/conda-build.git
 
 # Install the yum requirements defined canonically in the
 # "recipe/yum_requirements.txt" file. After updating that file,
