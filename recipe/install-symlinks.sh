fcf4c4444fce99453973688a3518e9627878d6d9
Isuru Fernando <isuruf@gmail.com>
2024-06-13 17:44:46 -0500

    Update install-symlinks.sh

--- recipe/install-symlinks.sh
+++ recipe/install-symlinks.sh
@@ -3,18 +3,18 @@
 source ${RECIPE_DIR}/setup_compiler.sh
 
 if [[ "$target_platform" == "win-"* ]]; then
-  symlink="cp"
+  symlink_or_copy="cp"
 else
-  symlink="ln -sf"
+  symlink_or_copy="ln -sf"
 fi
 
 if [[ "${PKG_NAME}" == "gcc" ]]; then
   for tool in cc cpp gcc gcc-ar gcc-nm gcc-ranlib gcov gcov-dump gcov-tool; do
-    $symlink ${PREFIX}/bin/${triplet}-${tool} ${PREFIX}/bin/${tool}
+    $symlink_or_copy ${PREFIX}/bin/${triplet}-${tool} ${PREFIX}/bin/${tool}
   done
 elif [[ "${PKG_NAME}" == "gxx" ]]; then
-  $symlink ${PREFIX}/bin/${triplet}-g++ ${PREFIX}/bin/g++
-  $symlink ${PREFIX}/bin/${triplet}-c++ ${PREFIX}/bin/c++
+  $symlink_or_copy ${PREFIX}/bin/${triplet}-g++ ${PREFIX}/bin/g++
+  $symlink_or_copy ${PREFIX}/bin/${triplet}-c++ ${PREFIX}/bin/c++
 elif [[ "${PKG_NAME}" == "gfortran" ]]; then
-  $symlink ${PREFIX}/bin/${triplet}-gfortran ${PREFIX}/bin/gfortran
+  $symlink_or_copy ${PREFIX}/bin/${triplet}-gfortran ${PREFIX}/bin/gfortran
 fi
