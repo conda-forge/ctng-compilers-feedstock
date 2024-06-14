346cdc2805c6444675ee8b5d7f5ffcd5a5d7be44
Isuru Fernando <isuruf@gmail.com>
2024-06-13 23:34:47 -0500

    use exe ext

--- recipe/install-symlinks.sh
+++ recipe/install-symlinks.sh
@@ -10,11 +10,11 @@ fi
 
 if [[ "${PKG_NAME}" == "gcc" ]]; then
   for tool in cc cpp gcc gcc-ar gcc-nm gcc-ranlib gcov gcov-dump gcov-tool; do
-    $symlink_or_copy ${PREFIX}/bin/${triplet}-${tool} ${PREFIX}/bin/${tool}
+    $symlink_or_copy ${PREFIX}/bin/${triplet}-${tool}${EXEEXT} ${PREFIX}/bin/${tool}${EXEEXT}
   done
 elif [[ "${PKG_NAME}" == "gxx" ]]; then
-  $symlink_or_copy ${PREFIX}/bin/${triplet}-g++ ${PREFIX}/bin/g++
-  $symlink_or_copy ${PREFIX}/bin/${triplet}-c++ ${PREFIX}/bin/c++
+  $symlink_or_copy ${PREFIX}/bin/${triplet}-g++${EXEEXT} ${PREFIX}/bin/g++${EXEEXT}
+  $symlink_or_copy ${PREFIX}/bin/${triplet}-c++${EXEEXT} ${PREFIX}/bin/c++${EXEEXT}
 elif [[ "${PKG_NAME}" == "gfortran" ]]; then
-  $symlink_or_copy ${PREFIX}/bin/${triplet}-gfortran ${PREFIX}/bin/gfortran
+  $symlink_or_copy ${PREFIX}/bin/${triplet}-gfortran${EXEEXT} ${PREFIX}/bin/gfortran${EXEEXT}
 fi
