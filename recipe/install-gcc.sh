bcb1c8cc434b90fc35aa53bad19b232a70f9add1
Isuru Fernando <isuruf@gmail.com>
2024-06-14 16:10:59 -0500

    fix install

--- recipe/install-gcc.sh
+++ recipe/install-gcc.sh
@@ -47,27 +47,27 @@ pushd ${SRC_DIR}/build
   make -C gcc prefix=${PREFIX} install-mkheaders
 
   if [[ -d ${TARGET}/libgomp ]]; then
-    make -C ${TARGET}/libgomp prefix=${PREFIX} install-nodist_{libsubinclude,toolexeclib}{HEADERS,LIBRARIES}
+    make -C ${TARGET}/libgomp prefix=${PREFIX} install
   fi
 
   if [[ -d ${TARGET}/libitm ]]; then
-    make -C ${TARGET}/libitm prefix=${PREFIX} install-nodist_toolexeclib{HEADERS,LIBRARIES}
+    make -C ${TARGET}/libitm prefix=${PREFIX} install
   fi
 
   if [[ -d ${TARGET}/libquadmath ]]; then
-    make -C ${TARGET}/libquadmath prefix=${PREFIX} install-nodist_libsubinclude{HEADERS,LIBRARIES}
+    make -C ${TARGET}/libquadmath prefix=${PREFIX} install
   fi
 
   if [[ -d ${TARGET}/libsanitizer ]]; then
-    make -C ${TARGET}/libsanitizer prefix=${PREFIX} install-nodist_{saninclude,toolexeclib}{HEADERS,LIBRARIES}
+    make -C ${TARGET}/libsanitizer prefix=${PREFIX} install
   fi
 
   if [[ -d ${TARGET}/libsanitizer/asan ]]; then
-    make -C ${TARGET}/libsanitizer/asan prefix=${PREFIX} install-nodist_toolexeclib{HEADERS,LIBRARIES}
+    make -C ${TARGET}/libsanitizer/asan prefix=${PREFIX} install
   fi
 
   if [[ -d ${TARGET}/libsanitizer/tsan ]]; then
-    make -C ${TARGET}/libsanitizer/tsan prefix=${PREFIX} install-nodist_toolexeclib{HEADERS,LIBRARIES}
+    make -C ${TARGET}/libsanitizer/tsan prefix=${PREFIX} install
   fi
 
   make -C libiberty prefix=${PREFIX} install
