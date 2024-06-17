21f650b196ab7ecf17e2eb6a548e052fa825b8e8
Isuru Fernando <isuruf@gmail.com>
2024-06-17 14:08:52 -0500

    fix asan build

--- recipe/install-gcc.sh
+++ recipe/install-gcc.sh
@@ -223,6 +223,8 @@ if [[ "$target_platform" == "$cross_target_platform" ]]; then
     if [[ -f "${PREFIX}/lib/lib${lib}.a" ]]; then
      mv ${PREFIX}/lib/lib${lib}.*a ${PREFIX}/lib/gcc/${TARGET}/${gcc_version}/
     fi
+  done
+  for lib in asan atomic gomp hwasan itm lsan quadmath tsan ubsan stdc++ gcc_s; do
     if [[ -f "${PREFIX}/lib/lib${lib}.so" ]]; then
      # install a shared library here since the directory ${PREFIX}/lib/gcc/${TARGET}/${gcc_version}
      # has the highest preference and we want shared libraries to have the highest preference
@@ -242,7 +244,6 @@ else
      mv ${PREFIX}/${TARGET}/lib/lib${lib}.*a ${PREFIX}/lib/gcc/${TARGET}/${gcc_version}/
     fi
     if [[ -f "${PREFIX}/${TARGET}/lib/lib${lib}.so" ]]; then
-     rm ${PREFIX}/${TARGET}/lib/lib${lib}.so
      ln -sf ${PREFIX}/${TARGET}/lib/lib${lib}.so ${PREFIX}/lib/gcc/${TARGET}/${gcc_version}/
     fi
   done
