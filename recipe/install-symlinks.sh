3bb79961532016c723965cc63b0d7be4a0af703a
Isuru Fernando <isuruf@gmail.com>
2026-02-03 16:11:50 -0800

    cc, cpp, c++ only for non-darwin to avoid conflicts with clang

--- recipe/install-symlinks.sh
+++ recipe/install-symlinks.sh
@@ -8,13 +8,21 @@ else
   symlink_or_copy="ln -sf"
 fi
 
+TOOLS=""
 if [[ "${PKG_NAME}" == "gcc" ]]; then
-  for tool in cc cpp gcc gcc-ar gcc-nm gcc-ranlib gcov gcov-dump gcov-tool; do
-    $symlink_or_copy ${PREFIX}/bin/${TARGET}-${tool}${EXEEXT} ${PREFIX}/bin/${tool}${EXEEXT}
-  done
+  TOOLS="gcc gcc-ar gcc-nm gcc-ranlib gcov gcov-dump gcov-tool"
+  if [[ "${HOST}" != *darwin* ]]; then
+    TOOLS="${TOOLS} cc cpp"
+  fi
 elif [[ "${PKG_NAME}" == "gxx" ]]; then
-  $symlink_or_copy ${PREFIX}/bin/${TARGET}-g++${EXEEXT} ${PREFIX}/bin/g++${EXEEXT}
-  $symlink_or_copy ${PREFIX}/bin/${TARGET}-c++${EXEEXT} ${PREFIX}/bin/c++${EXEEXT}
+  TOOLS="g++"
+  if [[ "${HOST}" != *darwin* ]]; then
+    TOOLS="${TOOLS} c++"
+  fi
 elif [[ "${PKG_NAME}" == "gfortran" ]]; then
-  $symlink_or_copy ${PREFIX}/bin/${TARGET}-gfortran${EXEEXT} ${PREFIX}/bin/gfortran${EXEEXT}
+  TOOLS="gfortran"
 fi
+
+for tool in ${TOOL}; do
+  $symlink_or_copy ${PREFIX}/bin/${TARGET}-${tool}${EXEEXT} ${PREFIX}/bin/${tool}${EXEEXT}
+done
